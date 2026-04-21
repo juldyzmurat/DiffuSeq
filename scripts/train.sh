db76b4dc1ef6907b977661e762762715c420fcea
Zhuldyz Ualikhnkyzy <zhuldyz@skoll.tsc.uc3m.es>
2026-04-21 21:28:40 +0200

    trained baseline

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,16 +1,15 @@
-CUDA_VISIBLE_DEVICES=2,3 python -m torch.distributed.launch --nproc_per_node=2 --master_port=12233 --use_env run_train.py \
+CUDA_VISIBLE_DEVICES=0,2,3 python -m torch.distributed.launch --nproc_per_node=3 --master_port=12233 --use_env run_train.py \
 --diff_steps 2000 \
 --lr 0.0001 \
---learning_steps 140000 \
---save_interval 20000 \
+--learning_steps 50000 \
+--save_interval 10000 \
 --seed 102 \
 --noise_schedule sqrt \
 --hidden_dim 128 \
 --bsz 2048 \
---microbatch 64 \
---dataset dialogue \
---data_dir datasets/CommonsenseConversation \
+--dataset qqp \
+--data_dir datasets/QQP \
 --vocab bert \
 --seq_len 128 \
 --schedule_sampler lossaware \
---notes dialogue
+--notes qqp
