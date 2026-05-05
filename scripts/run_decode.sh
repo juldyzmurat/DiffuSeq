7bc9e9f388dce4ce07108bcd32301587948e4fb1
Zhuldyz Ualikhnkyzy <zhuldyz@skoll.tsc.uc3m.es>
2026-05-05 08:31:05 +0200

    reconstruct

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,5 +1,5 @@
 python -u run_decode.py \
---model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260418-19:29:44 \
---pattern ema_0.9999_030000 \
+--model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260503-19:48:44 \
+--pattern ema_0.9999_001000 \
 --seed 123 \
 --split test
