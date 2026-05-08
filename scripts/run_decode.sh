242749bec81b8c520107fd3907ad4506b424dfb6
Zhuldyz Ualikhnkyzy <zhuldyz@skoll.tsc.uc3m.es>
2026-05-08 10:46:13 +0200

    need to rerun inference

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,5 +1,5 @@
 python -u run_decode.py \
---model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260418-19:29:44 \
---pattern ema_0.9999_030000 \
+--model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260504-19:40:15/ \
+--pattern ema_0.9999_005000 \
 --seed 123 \
 --split test
