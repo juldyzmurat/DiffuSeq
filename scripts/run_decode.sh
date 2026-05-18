3b6e243ef69b52c65eb1b4c6a478f40e847f80ac
Zhuldyz Ualikhnkyzy <zhuldyz@skoll.tsc.uc3m.es>
2026-05-18 18:05:42 +0200

    inference done

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,5 +1,5 @@
 python -u run_decode.py \
---model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260503-19:48:44 \
---pattern ema_0.9999_001000 \
+--model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260515-15:53:34 \
+--pattern ema_0.9999_002500 \
 --seed 123 \
 --split test
