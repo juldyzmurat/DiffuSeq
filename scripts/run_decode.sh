9386ef356f2f96db8ae9bdd6ea6fc335c5b50a65
Zhuldyz Ualikhnkyzy <zhuldyz@skoll.tsc.uc3m.es>
2026-05-30 15:07:45 +0200

    baseline undertrained

--- scripts/run_decode.sh
+++ scripts/run_decode.sh
@@ -1,5 +1,5 @@
 python -u run_decode.py \
---model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260517-11:14:29/ \
---pattern ema_0.9999_003500 \
+--model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_qqp20260527-14:11:28/ \
+--pattern ema_0.9999_003000 \
 --seed 123 \
 --split test
