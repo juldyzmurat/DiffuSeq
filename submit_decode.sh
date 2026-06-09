ab854844ffe6fcbfd9ff2d650c6fe3aee4e6732f
juldyzmurat <Juldyz.murat.murat5@gmail.com>
2026-06-09 09:40:01 +0200

    train logs

--- submit_decode.sh
+++ submit_decode.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH --account=uc3m-gts_c3_cluster_1-12
-#SBATCH --partition=gpu-batch
-#SBATCH --nodelist=srvgpu05
+#SBATCH --partition=batch
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:nvidia_a40:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=0-04:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --job-name=diffuseq-qqp-decode
 #SBATCH --output=/lustre/uc3m/gts_c3_cluster_1-12/zualikha/logs/%j.out
 #SBATCH --error=/lustre/uc3m/gts_c3_cluster_1-12/zualikha/logs/%j.err
@@ -17,7 +15,7 @@ conda activate /lustre/uc3m/gts_c3_cluster_1-12/zualikha/envs/diffuseq
 
 cd /home/zualikha/DiffuSeq/scripts
 python -u run_decode.py \
-  --model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_test-qqp-ecctriple20260531-00:50:24 \
-  --pattern ema_0.9999_005000 \
+  --model_dir diffusion_models/diffuseq_qqp_h128_lr0.0001_t2000_sqrt_lossaware_seed102_test-qqp-MSEtarget_restart20260608-14:31:07 \
+  --pattern ema_0.9999_025000 \
   --seed 123 \
   --split test 
